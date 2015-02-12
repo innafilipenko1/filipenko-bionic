f965a5ef0497a432e6d1f9bb2f40d258951f8e4d
innafilipenko1 <inna.filipenko@gmail.com>
2015-02-12 19:57:02 +0200

    Added readWordInFile to Test1.

--- src/test/java/class3/Test1.java
+++ src/test/java/class3/Test1.java
@@ -3,56 +3,33 @@ package class3;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
-import java.io.BufferedReader;
-import java.io.File;
-import java.io.FileReader;
 import java.io.IOException;
-import java.util.Scanner;
+
+import static java.nio.file.Files.readAllBytes;
+import static java.nio.file.Paths.get;
 
 /**
  * Created by c2611 on 11.02.2015.
  */
 
-
 public class Test1 {
 
     @Test
-    public void someMethod(){
-    }
+    public void readWordInFile() {
+        /*
+        Check if the file content contains the word given.
+        */
 
-    @Test
-    public void readWordInFile(){
-
-        BufferedReader br = null;
         String string = "test";
-        //File file = new File(args[0]);
 
         try {
 
-            String sCurrentLine;
+            String content = new String(readAllBytes(get("C://Users/ifilipenko/Downloads/testing.txt")));
 
-            br = new BufferedReader(new FileReader("D:\\testing.txt"));
-
-            while ((sCurrentLine = br.readLine()) != null) {
-                sCurrentLine = br.toString();
-                System.out.println(sCurrentLine);
-            }
-
-           // String content = new Scanner(file).useDelimiter("\\Z").next();
-
-            Assert.assertEquals(string, sCurrentLine);
+            Assert.assertTrue(content.contains(string));
 
         } catch (IOException e) {
             e.printStackTrace();
-        } finally {
-            try {
-                if (br != null)br.close();
-            } catch (IOException ex) {
-                ex.printStackTrace();
-            }
         }
-
-
-
     }
-  }
+}
