2499481a566eb65c0c1cd0ac22bd830033d47dc6
innafilipenko1 <inna.filipenko@gmail.com>
2015-02-23 17:58:19 +0200

    OLX. Simple Search test added list to get table.

--- src/test/java/olx/config/SimpleSearchePage.java
+++ src/test/java/olx/config/SimpleSearchePage.java
@@ -2,7 +2,10 @@ package olx.config;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.firefox.FirefoxDriver;
+
+import java.util.List;
 
 /**
  * Created by inna on 2/22/15.
@@ -20,6 +23,7 @@ public class SimpleSearchePage {
     private By searchLookup = By.id("headerSearch");
     private By searchButton = By.id("submit-searchmain");
     private By resultTable = By.id("offers_table");
+    private By tableElement = By.xpath("//table[@id='offers_table']/tbody/tr");
 
 
     public void openSite(){
@@ -37,8 +41,26 @@ public class SimpleSearchePage {
 
     public void checkResultTable(){
 
+//http://stackoverflow.com/questions/6198947/how-to-get-text-from-each-cell-of-an-html-table
         driver.findElement(resultTable);
-        //http://stackoverflow.com/questions/6198947/how-to-get-text-from-each-cell-of-an-html-table
+        List<WebElement> offersTable = driver.findElement(resultTable).findElements(tableElement);
+        int row_num,col_num;
+        row_num=1;
+        for(WebElement trElement : offersTable)
+        {
+            List<WebElement> td_collection=trElement.findElements(By.xpath("td"));
+            System.out.println("NUMBER OF COLUMNS="+td_collection.size());
+            col_num=1;
+            for(WebElement tdElement : td_collection)
+            {
+                System.out.println("row # "+row_num+", col # "+col_num+ "text="+tdElement.getText());
+                col_num++;
+            }
+            row_num++;
+        }
+
+
+
 
     }
 
