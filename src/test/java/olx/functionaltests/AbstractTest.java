daf19d6e6e8bb86f2f1b494098fef5ade5ae29d6
innafilipenko1 <inna.filipenko@gmail.com>
2015-03-10 18:06:21 +0200

    Added Browser Factory.

--- src/test/java/olx/functionaltests/AbstractTest.java
+++ src/test/java/olx/functionaltests/AbstractTest.java
@@ -6,6 +6,7 @@ import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.annotations.AfterSuite;
 import org.testng.annotations.BeforeSuite;
 import webdriver.Browser;
+import webdriver.BrowserFactory;
 
 import java.io.File;
 import java.util.concurrent.TimeUnit;
@@ -14,15 +15,6 @@ import java.util.concurrent.TimeUnit;
  * Created by c2611 on 23.02.2015.
  */
 
-   /*
-             https://code.google.com/p/chromedriver/issues/detail?id=679
-             https://code.google.com/p/selenium/issues/detail?id=7954
-      
-             work -  "C:\\Users\\ifilipenko\\Dropbox\\GitHub\\tools\\webdriverwin\\chromedriver.exe"
-             mac -  "/Users/inna/Dropbox/GitHub/tools/webdrivermac/chromedriver"
-             bionic  - "D:\\Tools\\chromedriver.exe"
-
-     */
 
 public class AbstractTest {
 
@@ -31,16 +23,7 @@ public class AbstractTest {
     @BeforeSuite
     public void init(){
 
-
-        if (PropertyLoader.loadProperty("browser").equals("firefox"))
-            driver = new Browser(new FirefoxDriver());
-        else if (PropertyLoader.loadProperty("browser").equals("chrome")) {
-        File file = new File("C:\\Users\\ifilipenko\\Dropbox\\GitHub\\tools\\webdriverwin\\chromedriver.exe");
-          System.setProperty("webdriver.chrome.driver",file.getAbsolutePath());
-            driver = new Browser(new ChromeDriver());
-        }
-
-        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+        driver = BrowserFactory.create(PropertyLoader.loadProperty("browser"));
 
     }
 
---
+++ src/test/java/webdriver/BrowserFactory.java
@@ -0,0 +1,110 @@
+package webdriver;
+
+import olx.utils.PropertyLoader;
+import org.openqa.selenium.Dimension;
+import org.openqa.selenium.Platform;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.htmlunit.HtmlUnitDriver;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.testng.Assert;
+
+import java.util.concurrent.TimeUnit;
+
+/**
+ * Created by ifilipenko on 3/10/2015.
+ *
+ *
+ https://code.google.com/p/chromedriver/issues/detail?id=679
+ https://code.google.com/p/selenium/issues/detail?id=7954
+
+ work -  "C:\\Users\\ifilipenko\\Dropbox\\GitHub\\tools\\webdriverwin\\chromedriver.exe"
+ mac -  "/Users/inna/Dropbox/GitHub/tools/webdrivermac/chromedriver"
+ bionic  - "D:\\Tools\\chromedriver.exe"
+
+ */
+
+public class BrowserFactory {
+
+    /* Browsers constants */
+    public static final String CHROME = "chrome";
+    public static final String FIREFOX = "firefox";
+    public static final String HTML_UNIT = "htmlunit";
+
+    /* Platform constants */
+    public static final String WINDOWS = "windows";
+    public static final String MAC = "mac";
+
+    public static Browser create(String browser)
+    {
+        Browser testDriver = setDefaultConfiguration(browser);
+        return testDriver;
+    }
+
+    private static Browser setDefaultConfiguration(String browser)
+    {
+        WebDriver driver = null;
+        DesiredCapabilities capabilities = setDefaultCapabilities(browser);
+
+        if (browser.equals(FIREFOX))
+        {
+            driver = new FirefoxDriver(capabilities);
+        } else
+        if (browser.equals(CHROME))
+        {
+            //system.property (env variable) = "webdriver.chrome.driver"
+            System.setProperty("webdriver.chrome.driver","D:\\Selenium\\chromedriver.exe");
+            driver = new ChromeDriver(capabilities);
+        } else
+        if (browser.equals(HTML_UNIT))
+        {
+            driver = new HtmlUnitDriver(capabilities);
+        } else
+            Assert.fail("Browser configuration error");
+
+        //additional configuration
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+
+        return new Browser(driver);
+    }
+
+    private static WebDriver setScreenReolution(WebDriver driver)
+    {
+        int xDimension = Integer.parseInt(PropertyLoader.loadProperty("selenium.screen.x"));
+        int yDimension = Integer.parseInt(PropertyLoader.loadProperty("selenium.screen.y"));
+        driver.manage().window().setSize(new Dimension(xDimension,yDimension));
+        return driver;
+    }
+
+    private static DesiredCapabilities setDefaultCapabilities(String browser)
+    {
+        DesiredCapabilities capabilities = new DesiredCapabilities();
+        capabilities = setPlatform(capabilities,"");
+
+        if (browser.contains(FIREFOX)){
+            capabilities = DesiredCapabilities.firefox();
+        } else
+        if (browser.contains(CHROME)){
+            capabilities = DesiredCapabilities.chrome();
+        } else
+        if (browser.contains(HTML_UNIT)){
+            capabilities = DesiredCapabilities.htmlUnit();
+        }else
+            Assert.fail("No configuration available for driver");
+
+        return capabilities;
+    }
+
+    private static DesiredCapabilities setPlatform(DesiredCapabilities capabilities,String platformName)
+    {
+        if (platformName.equals(MAC))
+        {
+            capabilities.setPlatform(Platform.MAC);
+        } else
+        {
+            capabilities.setPlatform(Platform.WINDOWS);
+        }
+        return capabilities;
+    }
+}
